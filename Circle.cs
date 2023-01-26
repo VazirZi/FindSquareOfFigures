958967ec60c05c5da6bb3385bcf459cd2e11bed4
VazirZi <vazir.zivar@mail.com>
2023-01-26 21:05:27 +0500

    Circle

---
+++ Circle.cs
@@ -0,0 +1,13 @@
+class Circle : Figure
+    {
+        public Circle(double _r)
+        {
+            r = _r;
+        }
+        public double r { get; set; }
+        public override void FindSquare()
+        {
+            double result = (Math.Pow(r, 2)) * Math.PI;
+            GetQuantity(result);
+        }
+    }
