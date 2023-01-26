c61ae7532548233ea81559e1036c63f4fa556bdf
VazirZi <vazir.zivar@mail.com>
2023-01-26 21:05:07 +0500

    Square

---
+++ Square.cs
@@ -0,0 +1,15 @@
+class  Square : Figure
+    {
+        public Square(double _length, double _width)
+        {
+            length = _length;
+            width = _width;
+        }
+        public double length { get; set; }
+        public double width { get; set; }
+        public override void FindSquare()
+        {
+            double result = length * width;
+            GetQuantity(result);
+        }
+    }
