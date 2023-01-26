f3e61d3348edbc4059a590aa6600386ae7b1c626
VazirZi <120021080+VazirZi@users.noreply.github.com>
2023-01-26 21:26:47 +0500

    Update Rectangle.cs

--- Rectangle.cs
+++ Rectangle.cs
@@ -1,9 +1,9 @@
 class Rectangle : Square
-    {
-        public Rectangle(double _length, double _width) : base(_length, _width){}
+{
+    public Rectangle(double _length, double _width) : base(_length, _width){}
     
-        public override void FindSquare()
-        {
-            base.FindSquare();
-        }
+    public override void FindSquare()
+    {
+        base.FindSquare();
     }
+}
