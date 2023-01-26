f89a5ea99dee7f33388250def46330bd4109b9f2
VazirZi <120021080+VazirZi@users.noreply.github.com>
2023-01-26 21:25:32 +0500

    Update Circle.cs

--- Circle.cs
+++ Circle.cs
@@ -1,13 +1,13 @@
 class Circle : Figure
+{
+    public Circle(double _r)
     {
-        public Circle(double _r)
-        {
-            r = _r;
-        }
-        public double r { get; set; }
-        public override void FindSquare()
-        {
-            double result = (Math.Pow(r, 2)) * Math.PI;
-            GetQuantity(result);
-        }
+        r = _r;
     }
+    public double r { get; set; }
+    public override void FindSquare()
+    {
+        double result = (Math.Pow(r, 2)) * Math.PI;
+        GetQuantity(result);
+    }
+}
