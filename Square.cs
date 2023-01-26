ba3f49d473d8878356546f3dd049693cffe6c82c
VazirZi <120021080+VazirZi@users.noreply.github.com>
2023-01-26 21:27:20 +0500

    Update Square.cs

--- Square.cs
+++ Square.cs
@@ -1,15 +1,15 @@
 class  Square : Figure
+{
+    public Square(double _length, double _width)
     {
-        public Square(double _length, double _width)
-        {
-            length = _length;
-            width = _width;
-        }
-        public double length { get; set; }
-        public double width { get; set; }
-        public override void FindSquare()
-        {
-            double result = length * width;
-            GetQuantity(result);
-        }
+        length = _length;
+        width = _width;
     }
+    public double length { get; set; }
+    public double width { get; set; }
+    public override void FindSquare()
+    {
+        double result = length * width;
+        GetQuantity(result);
+    }
+}
